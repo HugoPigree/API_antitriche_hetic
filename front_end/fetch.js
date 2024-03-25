53716a44ace40b9ca5e8128211a3de425b7f12da
Hugopigree <h.pigree13012005@gmail.com>
2024-03-25 01:02:03 +0100

    oe

--- front_end/fetch.js
+++ front_end/fetch.js
@@ -1,12 +1,24 @@
-let headersList = {
-    "Accept": "*/*",
-    "User-Agent": "Thunder Client (https://www.thunderclient.com)"
-   }
-let response = await fetch("http://localhost:3000/api/v1/eleve", { 
-    method: "POST",
-    headers: headersList
-});
-   
-let data = await response.text();
-   console.log(data);
-   
+
+    document.addEventListener('DOMContentLoaded', function() {
+        const form = document.querySelector('form');
+        form.addEventListener('submit', async function(event) {
+            event.preventDefault();
+            const formData = new FormData(form);
+            try {
+                const response = await fetch('http://localhost:3000/api/v1/eleve', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({ nameInput: formData.get('nameInput') })
+                });
+                const data = await response.json();
+                console.log(data); // Afficher la réponse du serveur
+                // Rediriger l'utilisateur vers une autre page ou effectuer une autre action après l'ajout réussi
+            } catch (error) {
+                console.error(error);
+                // Gérer les erreurs de manière appropriée
+            }
+        });
+    });
+
--- front_end/script.js
+++ front_end/script.js
@@ -56,3 +56,30 @@ document.addEventListener('visibilitychange', function() {
   } 
 });
 
+
+document.addEventListener('DOMContentLoaded', function() {
+  const form = document.querySelector('form');
+  form.addEventListener('submit', async function(event) {
+      event.preventDefault();
+      const formData = new FormData(form);
+      try {
+          const response = await fetch('http://localhost:3000/api/v1/eleve', {
+              method: 'POST',
+              headers: {
+                  'Content-Type': 'application/json'
+              },
+              body: JSON.stringify({ nom: formData.get('nom') })
+          });
+          const data = await response.json();
+      } catch (error) {
+          console.error(error);
+      }
+  });
+});
+
+
+
+
+
+
+
